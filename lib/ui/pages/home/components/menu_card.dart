dc93ca8ab319a14d4e1bf9a54ed0d6e8e3c1d0f3
kelvia_kolln <kelviakolln19@hotmail.com>
2024-08-23 15:26:52 -0300

    criando a home page

---
+++ lib/ui/pages/home/components/menu_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+
+class MenuCard extends StatelessWidget {
+  final IconData icon;
+  final String translate;
+  const MenuCard({required this.icon, required this.translate, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 70,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.teal.withOpacity(0.4)),
+          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(icon, color: Colors.teal, size: 22),
+              const SizedBox(width: 10),
+              Text(FlutterI18n.translate(context, 'home.$translate'), style: const TextStyle(fontSize: 22, color: Colors.black)),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: const Icon(Icons.arrow_right_outlined,
+                      color: Colors.teal, size: 32),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'components/components.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.teal,
+        title: const Text(
+          'Mini ERP',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MenuCard(icon: Icons.person_2_outlined, translate: 'custumers'),
+          MenuCard(icon: Icons.shopping_bag_outlined, translate: 'protucts'),
+          MenuCard(icon: Icons.shopping_cart_outlined, translate: 'orders'),
+          MenuCard(icon: Icons.store_outlined, translate: 'suppliers'),
+        ],
+      ),
+    );
   }
 }
